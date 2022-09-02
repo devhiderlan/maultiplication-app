03075126977a3585f09c51519b471f9ba1d5850a
devhiderlan <dutrahiderlan@gmail.com>
2022-09-02 15:55:13 -0300

    Criada função para validar resposta do usuário, e criado outra função para armazenar resposta do usuário no local storage

---
+++ Assets/script.js
@@ -0,0 +1,35 @@
+const num1 = Math.ceil(Math.random()*10)
+const num2 = Math.ceil(Math.random()*10)
+
+const question = document.getElementById('question')
+const input = document.getElementById('input')
+const form = document.getElementById('form')
+const scoreEl = document.getElementById('score')
+
+let score = JSON.parse(localStorage.getItem('score'))
+
+if (!score) {
+  score = 0
+}
+
+scoreEl.innerText = `score: ${score}`
+question.innerText = `Whats is ${num1} multiply by ${num2} ?`
+
+const correctAnswer = num1 * num2;
+
+form.addEventListener('submit', () => {
+  const userAnswer = +input.value
+  if (userAnswer === correctAnswer) {
+    score++
+    updateLocalStorage()
+  } else{
+    score--
+    updateLocalStorage()
+  }
+})
+
+function updateLocalStorage () {
+  localStorage.setItem('score', JSON.stringify(score));
+}
+
+
